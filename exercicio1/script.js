89a50874111106a081af108a7a2d7782f119d0d6
Arthuramaral100 <arthuramaral57@gmail.com>
2022-09-01 15:44:37 -0300

    Exercicios da aula 010-arrays

---
+++ exercicio1/script.js
@@ -0,0 +1,13 @@
+//O primeiro deve possuir apenas **números**, e estes números não devem seguir nenhuma ordem específica;
+const arrayNumber = [1, 56, 3, 8, 100, 23]
+
+//O segundo array deve possuir apenas **strings**;
+const arrayStrings = ["batata", "arroz", "cenoura", "banana"] 
+
+//O terceiro array deve possuir **números**, **strings** e **booleanos**.
+const arrayMisto = [56, "bolo", 30, false, "tomate"] 
+
+//Imprima cada um dos arrays acima, utilizando `console.log()`
+ console.log(arrayNumber);
+ console.log(arrayStrings);
+ console.log(arrayMisto);
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,19 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const arrayNumber = [1, 56, 3, 8, 100, 23]
+const arrayStrings = ["batata", "arroz", "cenoura", "banana"]
+const arrayMisto = [56, "bolo", "torta", 30, false, "tomate"]
+
+//Imprima a quantidade de itens de cada array (utilize um console.log() para cada impressão);
+console.log("Itens da arrayNumber: ", arrayNumber.length);
+console.log("Itens da arrayStrings: ", arrayStrings.length);
+console.log("Itens da arrayMisto: ", arrayMisto.length);
+
+//Imprima o primeiro item do primeiro array, o segundo item do segundo array, e o terceiro item do terceiro array;
+console.log("Primeiro item do primeiro Array: ", arrayNumber[0]);
+console.log("Segundo item do segundo Array: ", arrayStrings[1]);
+console.log("Terceiro item do terceiro Array: ", arrayMisto[2]);
+
+//Imprima uma informação booleana verdadeira sobre a **inclusão** de um item do primeiro array, e uma informação booleana falsa sobre a **inclusão** de um item ao terceiro array.
+console.log("Na primeira array tem o número 100?", arrayNumber.includes(100));
+console.log("Na terceira array tem a string \"chocolate\" ?", arrayMisto.includes("chocolate"));
+
+
